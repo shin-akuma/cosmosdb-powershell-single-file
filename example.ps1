623fc32747089229b0c70d58acf1c1158cf0c6cb
shin-akuma <ilovexiaozhao@gmail.com>
2024-07-24 15:53:43 +1200

    Create example.ps1

---
+++ example.ps1
@@ -0,0 +1,20 @@
+. "$PSScriptRoot\query-cosmos.ps1"
+
+$CosmosDBEndPoint = "https://xxxx.documents.azure.com:443/"
+$DatabaseId = "xxx"
+$CollectionId = "xxx"
+$MasterKey = $($env:cosmosKey)
+
+$Query = "SELECT * FROM c where c.status = 'active'"
+
+$Result = Query-CosmosDb -EndPoint $CosmosDBEndPoint -DataBaseId $DataBaseId -CollectionId $CollectionId -MasterKey $MasterKey -Query $mapQuery
+
+$obj = @{
+  id = $(New-Guid).guid
+  pk = 'pk1'
+  status = 'active'
+}
+
+New-Document -EndPoint $CosmosDBEndPoint -DataBaseId $DataBaseId -ContainerId $CollectionId -MasterKey $MasterKey -ItemId $obj.id -ItemDefinition $($obj | ConvertTo-Json) -PartitionKey $obj.pk
+
+Update-Document -EndPoint $CosmosDBEndPoint -DataBaseId $DataBaseId -ContainerId $CollectionId -MasterKey $MasterKey -ItemId $obj.id -ItemDefinition $($obj | ConvertTo-Json) -PartitionKey $obj.pk
